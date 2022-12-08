bbd7cf1bfe8b0af9c6fa79dd23f3a40d7cf769e1
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2022-12-08 09:54:37 +0200

    Refactored day 8

--- Day8.fs
+++ Day8.fs
@@ -2,64 +2,62 @@
 
 open AoC2022.Utils
 
-let getForest fn =
+let makeForest fn =
     let input = readInput fn
 
-    let trees =
+    let forest =
         input
         |> Array.ofSeq
         |> Array.map (Array.ofSeq >> Array.map (string >> int))
 
-    let transposed = trees |> Array.transpose
-    let maxX = Array.length trees
-    let maxY = Array.length (Array.head trees)
-    trees, transposed, maxX, maxY
+    let maxX = Array.length forest
+    let maxY = Array.length (Array.head forest)
+    forest, maxX, maxY
+
+let getTreesInLineOfSight (forest: int array array) x y maxX maxY =
+    let transposed = forest |> Array.transpose
+    let north = transposed.[x][0 .. y - 1] |> Array.rev
+    let west = forest.[y][0 .. x - 1] |> Array.rev
+    let east = forest.[y][x + 1 .. maxX]
+    let south = transposed.[x][y + 1 .. maxY]
+    seq [ north; west; east; south ]
 
 let part1 fn () =
 
-    let trees, transposed, maxX, maxY = getForest fn
+    let forest, maxX, maxY = makeForest fn
 
     let getTallest =
         function
         | [||] -> -1
         | a -> Array.max a
 
-    let innerTrees =
-        seq {
-            for y in [ 0 .. maxX - 1 ] do
-                for x in [ 0 .. maxY - 1 ] do
-                    let tree = trees.[y][x]
-                    let west = trees.[y][0 .. x - 1] |> getTallest
-                    let east = trees.[y][x + 1 .. maxX] |> getTallest
-                    let north = transposed.[x][0 .. y - 1] |> getTallest
-                    let south = transposed.[x][y + 1 .. maxY] |> getTallest
-                    let los = [ north; west; east; south ] |> List.min
-                    yield los < tree
-        }
-        |> Seq.filter id
-        |> Seq.length
-        |> int64
+    seq {
+        for y in [ 0 .. maxX - 1 ] do
+            for x in [ 0 .. maxY - 1 ] do
+                let tree = forest.[y][x]
 
-    innerTrees
+                yield
+                    (getTreesInLineOfSight forest x y maxX maxY
+                     |> Seq.map getTallest
+                     |> Seq.min) < tree
+    }
+    |> Seq.filter id
+    |> Seq.length
+    |> int64
 
 
 let part2 fn () =
 
-    let trees, transposed, maxX, maxY = getForest fn
+    let forest, maxX, maxY = makeForest fn
 
     seq {
         for y in [ 1 .. maxX - 2 ] do
             for x in [ 1 .. maxY - 2 ] do
-                let tree = trees.[y][x]
-                let west = trees.[y][0 .. x - 1] |> Array.rev
-                let east = trees.[y][x + 1 .. maxX]
-                let north = transposed.[x][0 .. y - 1] |> Array.rev
-                let south = transposed.[x][y + 1 .. maxY]
+                let tree = forest.[y][x]
 
-                let los =
-                    [ north; west; east; south ]
-                    |> List.map (Seq.takeWhilePlus1 (fun h -> h < tree) >> Seq.length >> int64)
-
-                yield los |> List.reduce (*)
+                yield
+                    getTreesInLineOfSight forest x y maxX maxY
+                    |> Seq.map (Seq.takeWhilePlus1 (fun h -> h < tree) >> Seq.length >> int64)
+                    |> Seq.reduce (*)
     }
     |> Seq.max
