83bbe356c41b83744b7da6b765e9024286beb5c0
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2022-12-12 16:16:21 +0200

    Trying again with A* algorithm

--- Day12.fs
+++ Day12.fs
@@ -2,129 +2,85 @@
 
 open AoC2022.Utils
 
-type Node =
-    { X: int
-      Y: int
-      Height: uint64
-      Distance: uint64
-      Previous: option<Node> }
 
-
-let createNode x y ht =
-    { X = x
-      Y = y
-      Height =  ht
-      Distance = System.UInt64.MaxValue
-      Previous = None }
-
-let getAllNodes (grid: Node [] []) =
-    grid
-    |> Seq.concat
-    |> Seq.map (fun n -> n.X, n.Y)
-    |> Set.ofSeq
-
-let getNode (grid: (Node) [] []) (x, y) () = grid.[x].[y]
-
-    
+(*
 let isValidMove (grid: Node[][]) node x y =
     let heigthDifference = (node.Height - (getNode grid (x,y) ()).Height)
     heigthDifference <= 1UL && heigthDifference >= 0UL
-    
+*)
 
 
 let printRow row =
     row
-    |> Array.map
-        (fun n ->
-            match n.Distance with
-            | System.UInt64.MaxValue -> " .. "
-            | n -> n |> sprintf "%3d ")
+    |> List.map (sprintf "%3d ")
     |> String.concat ""
     |> printfn "%s"
 
 let printGrid grid =
-    grid |> Array.iter printRow
+    grid |> List.iter printRow
     printfn ""
-let getNeighbourCoords x y =
-    seq [ x - 1, y
-          x, y - 1
-          x + 1, y
-          x, y + 1 ]
 
-let rec dijkstra (grid: Node [] []) queue =
-    match Set.isEmpty queue with
-    | true -> grid
-    | false ->
-        let curr =
-            queue
-            |> Seq.minBy (fun (x, y) -> grid.[x].[y].Distance)
-            |> getNode grid
-            <| ()
+let getNeighbourCoords (x,y) () = seq [ x - 1, y; x, y - 1; x + 1, y; x, y + 1 ]
 
-        let queue' = Set.remove (curr.X, curr.Y) queue
-
-        let neighbours =
-            getNeighbourCoords curr.X curr.Y
-            |> Seq.filter
-                (fun (nx, ny) ->
-                    nx >= 0
-                    && ny >= 0
-                    && nx < Array.length grid
-                    && ny < Array.length (Array.head grid)
-                    && (queue' |> Set.contains (nx, ny))
-                    && (isValidMove grid curr nx ny)
-            )
-            |> Seq.map (fun (x', y') -> getNode grid (x', y') ())
-
-        neighbours
-        |> Seq.iter
-            (fun n ->
-                let newDistance = curr.Distance + 1UL
-
-                if newDistance < n.Distance then
-
-                    grid.[n.X].[n.Y] <-
-                        { n with
-                              Distance = newDistance
-                              Previous = Some curr })
-
-        dijkstra grid queue'
-
-let charToUL (c:char) =
+let charToUL (c: char) =
     match c with
     | 'E' -> 27UL
     | 'S' -> 0UL
     | c' -> uint64 c' - uint64 'a' + 1UL
+
 let loadGrid fn =
-    let parseRow x row =
-        row
-        |> Seq.mapi (fun y c -> c |> charToUL |> createNode x y)
-        |> Array.ofSeq
+    let parseRow x row = row |> Seq.mapi (fun y c -> c |> charToUL) |> List.ofSeq
 
     let grid =
         readInput fn
         |> Seq.mapi (fun x row -> row |> parseRow x)
-        |> Array.ofSeq
-    printGrid grid
+        |> List.ofSeq
+
     grid
 
-let zeroStart (grid: Node [] []) =
-    grid.[0].[0] <-
-        { grid.[0].[0] with
-              Height = 0UL
-              Distance = 0UL }
 
-
+let findCoodsByValue grid value = 
+    let x = grid |> List.findIndex (fun l -> l |> List.contains value)
+    let y = grid.[x] |> List.findIndex (fun i -> i = value)
+    x, y
 
 let part1 fn () =
     let grid = loadGrid fn
-    zeroStart grid
 
-    dijkstra grid (getAllNodes grid)
-     |> printGrid
-     
-    0L
+    let width, height = List.length grid.[0], List.length grid
 
-    
+    let start = findCoodsByValue grid 0UL
+    let goal = findCoodsByValue grid 27UL
+    // printGrid grid
+    printfn $"Start %A{start}"
+    printfn $"Goal %A{goal}"
+
+    let neighbours (x, y) =
+        let ret = getNeighbourCoords (x,y) () |> Seq.filter (fun (nx, ny) ->
+            nx >= 0
+            && ny >= 0
+            && nx < height 
+            && ny < width 
+            && match grid.[nx].[ny] - grid.[x].[y] with
+               | hd when hd < 2UL -> true
+               | _ -> false)
+        printfn $"Neighbours for [%d{x}, %d{y}] = %A{ret}"
+        ret
+
+    let gScore _ _ = 1.
+    let fScore (x, y) (gx, gy) = sqrt ((float gx - float x) ** 2. + (float gy - float y) ** 2.)
+
+    match
+        AStar.search
+            start
+            goal
+            { neighbours = neighbours
+              gCost = gScore
+              fCost = fScore
+              maxIterations = None }
+    with
+    | Some path ->
+        path |> Seq.length |> int64 |> (+) -1L
+    | None -> 0L
 
 let part2 fn () = 0L
