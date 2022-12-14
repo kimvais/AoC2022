50ac5bc96cf5d4bc9e004e3a1249b7a696494c3f
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2022-12-14 09:41:58 +0200

    Minor refactoring

--- Day14.fs
+++ Day14.fs
@@ -7,35 +7,40 @@ let parseCoords (s: string) =
     |> Array.map int64
     |> (fun a -> Seq.head a, Seq.last a)
 
-let part1 fn () =
+let makeBlocks ((xa: int64, ya: int64), (xb: int64, yb: int64)) =
+    let x1 = [ xa; xb ] |> Seq.min
+    let x2 = [ xa; xb ] |> Seq.max
+    let y1 = [ ya; yb ] |> Seq.min
+    let y2 = [ ya; yb ] |> Seq.max
+
+    match x1 = x2 with
+    | true -> [ y1..y2 ] |> List.map (fun y -> (x1, y))
+    | false -> [ x1..x2 ] |> List.map (fun x -> (x, y1))
+
+
+let parseInput fn =
     let input = readInput fn
 
-    let makeBlocks ((xa: int64, ya: int64), (xb: int64, yb: int64)) =
-        let x1 = [ xa; xb ] |> Seq.min
-        let x2 = [ xa; xb ] |> Seq.max
-        let y1 = [ ya; yb ] |> Seq.min
-        let y2 = [ ya; yb ] |> Seq.max
+    input
+    |> Seq.map (
+        splitS " -> "
+        >> List.ofArray
+        >> List.map parseCoords
+        >> List.pairwise
+        >> List.map makeBlocks
+        >> List.concat
+    )
+    |> Seq.concat
 
-        match x1 = x2 with
-        | true -> [ y1..y2 ] |> List.map (fun y -> (x1, y))
-        | false -> [ x1..x2 ] |> List.map (fun x -> (x, y1))
-
-
-    let lines =
-        input
-        |> Seq.map (
-            splitS " -> "
-            >> List.ofArray
-            >> List.map parseCoords
-            >> List.pairwise
-            >> List.map makeBlocks
-            >> List.concat
-        )
-        |> Seq.concat
-        |> Set.ofSeq
-
+let part1 fn () =
+    let lines = parseInput fn
+    let blocks = lines |> Set.ofSeq
+    let voidRight = lines |> Seq.maxBy (fst >> (+) 1L) |> fst
+    let voidLeft = lines |> Seq.minBy (fst >> (+) 1L) |> fst
+    
     printfn "%A" lines
-    printfn "%d" (Set.count lines)
+    printfn $"Left: %i{voidLeft} - Right: %i{voidRight}"
+    printfn "%d" (Set.count blocks)
     0L
 
 let part2 fn () = 0L
