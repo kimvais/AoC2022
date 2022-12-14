bb85dbec126f4e0fbc9e1b3b10ffd09d508fa14b
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2022-12-14 08:22:22 +0200

    Solve day 13

--- Day13.fs
+++ Day13.fs
@@ -20,14 +20,30 @@ let parseRecord r =
     let pair = r |> splitByLinefeed
     let left = parseList (Seq.head pair) |> parse
     let right = parseList (Seq.last pair) |> parse
-    match left, right with 
-    | List l, List r -> l, r
-    | _ -> failwith "Error: one of the lists is not a list!"
+    left, right
+
+let rec compare left right =
+    match left, right with
+    | Int l, Int r -> l.CompareTo r
+    | Int _, List _ -> compare (List [ left ]) right
+    | List _, Int _ -> compare left (List [ right ])
+    | List l, List r ->
+        Seq.zip l r
+        |> Seq.map (fun (a, b) -> compare a b)
+        |> Seq.skipWhile ((=) 0)
+        |> Seq.tryHead
+        |> function
+            | Some h -> h
+            | None -> (Seq.length l).CompareTo(Seq.length r)
 
 let part1 fn () =
     let input = readInputDelimByEmptyLine fn
-    let records = input |> Array.map parseRecord
-    printfn "%A" records
-    0L
+
+    input
+    |> Array.map parseRecord
+    |> Array.mapi (fun i (l, r) -> (i + 1, compare l r))
+    |> Array.filter (snd >> (=) -1)
+    |> Array.sumBy fst
+    |> int64
 
 let part2 fn () = 0L
