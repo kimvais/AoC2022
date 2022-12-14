5ae41c523c6093a6918e53db6fcae6c1496b4644
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2022-12-14 08:55:05 +0200

    Placeholder for day 14

---
+++ Day14.fs
@@ -0,0 +1,8 @@
+﻿module AoC2022.Day14
+
+open AoC2022.Utils
+
+let part1 fn () = 0L
+
+let part2 fn () = 0L
+
--- Program.fs
+++ Program.fs
@@ -34,10 +34,10 @@ let main argv =
     | "12b" -> Day12.part2 "12" ()
     | "13" -> Day13.part1 "13" ()
     | "13b" -> Day13.part2 "13" ()
-    | "test" -> Day13.part1 "test13" ()
-    (*
     | "14" -> Day14.part1 "14" ()
     | "14b" -> Day14.part2 "14" ()
+    | "test" -> Day14.part1 "test14" ()
+    (*
     | "15" -> Day15.part1 "15" ()
     | "15b" -> Day15.part2 "15" ()
     | "16" -> Day16.part1 "16" ()
