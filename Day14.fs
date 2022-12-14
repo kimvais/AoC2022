fabedf88ab03dd53b65afb35c4be74d33872561f
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2022-12-14 09:34:31 +0200

    Parsing for day 14

--- Day14.fs
+++ Day14.fs
@@ -2,7 +2,40 @@
 
 open AoC2022.Utils
 
-let part1 fn () = 0L
+let parseCoords (s: string) =
+    s.Split(',')
+    |> Array.map int64
+    |> (fun a -> Seq.head a, Seq.last a)
+
+let part1 fn () =
+    let input = readInput fn
+
+    let makeBlocks ((xa: int64, ya: int64), (xb: int64, yb: int64)) =
+        let x1 = [ xa; xb ] |> Seq.min
+        let x2 = [ xa; xb ] |> Seq.max
+        let y1 = [ ya; yb ] |> Seq.min
+        let y2 = [ ya; yb ] |> Seq.max
+
+        match x1 = x2 with
+        | true -> [ y1..y2 ] |> List.map (fun y -> (x1, y))
+        | false -> [ x1..x2 ] |> List.map (fun x -> (x, y1))
+
+
+    let lines =
+        input
+        |> Seq.map (
+            splitS " -> "
+            >> List.ofArray
+            >> List.map parseCoords
+            >> List.pairwise
+            >> List.map makeBlocks
+            >> List.concat
+        )
+        |> Seq.concat
+        |> Set.ofSeq
+
+    printfn "%A" lines
+    printfn "%d" (Set.count lines)
+    0L
 
 let part2 fn () = 0L
-
