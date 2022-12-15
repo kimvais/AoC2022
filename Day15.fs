81da872202ba2d74c69600160702103619e2d936
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2022-12-15 15:53:32 +0200

    Find gaps by recursing

--- Day15.fs
+++ Day15.fs
@@ -29,33 +29,73 @@ let parse fn =
 
 let findSensorsWithinRange row sensors = sensors |> Seq.filter (fun s -> (abs (s.Y - row) <= s.Range))
 
-let part1 rowNo fn () =
-    let coords = parse fn
-    // printfn $"%A{coords}"
-    // let distances = coords |> Seq.map getDistance
-    // printfn $"%A{distances}"
-
+let getCoverage coords rowNo =
     let sensors =
         coords
         |> Seq.map (fun c -> (fst c, (getDistance >> getRange) <| c) |> makeSensor)
 
     let withInRange = sensors |> findSensorsWithinRange rowNo
-    // printfn $"%A{sensors}"
-    // printfn $"%A{withInRange |> List.ofSeq}"
 
-    let coverage =
-        withInRange
-        |> Seq.map (fun s -> s.X, s.Range - (abs (s.Y - rowNo)))
-        |> Seq.map (fun (x, range) -> [ x - range; x + range ])
-        |> Seq.sort 
+    withInRange
+    |> Seq.map (fun s -> s.X, s.Range - (abs (s.Y - rowNo)))
+    |> Seq.map (fun (x, range) -> (x - range, x + range))
 
-    // let beaconsOnRow = coords |> Seq.map snd |> Seq.filter (fun (_,y) -> y = rowNo) |> Set.ofSeq |> Set.count |> int64
-    // printfn $"%d{beaconsOnRow}"
+let part1 rowNo fn () =
+    let coords = parse fn
+    let coverage = getCoverage coords rowNo
     printfn $"%A{coverage |> List.ofSeq}"
-    let first = coverage |> Seq.minBy Seq.head |> Seq.head
-    let last = coverage |> Seq.maxBy Seq.last |> Seq.last
-    // coverage |> Set.count |> int64 |> (+) -beaconsOnRow
+    let first = coverage |> Seq.minBy fst |> fst
+    let last = coverage |> Seq.maxBy snd |> snd
     printfn $"%A{first} %A{last}"
     last - first
-    
-let part2 fn () = 0L
+
+let folder (s: int64, e: int64, gap: int64 option) ((l1: int64, r1: int64), (l2: int64, r2: int64)) =
+    let s' = Seq.min [ s; l1; l2 ]
+
+    let gap' =
+        match l2 - r1 with
+        | n when n >= 0L ->
+            match gap with
+            | Some s -> Some s
+            | None -> Some r1
+        | _ -> None
+
+    s', r2, gap'
+
+let findGap (ranges: (int64 * int64) seq) : int64 * int64 * int64 option =
+    let ranges' = ranges |> Seq.pairwise
+    ranges' |> Seq.fold folder (0, 20L, None)
+   
+let rec gapFinder (n:int64) (rem: (int64 * int64) list) =
+    match rem with
+    | [] -> None
+    | l ->
+        let candidates  = l |> List.sortBy fst |> List.takeWhile (fun t -> (fst t) <= n) |> List.sortByDescending snd
+        match candidates with
+        | [] -> Some n
+        | c ->
+            let next = c |> List.head |> snd
+            let rem' = l |> List.filter (fun t -> (fst t) >= next)
+            printfn $"%d{next} %A{rem'}"
+            gapFinder next rem'
+        
+
+let part2 fn () =
+    let coords = parse fn
+    (*
+    let coverage =
+        [ 0L .. 20L ]
+        |> Seq.map (fun cov -> getCoverage coords cov |> findGap)
+
+    coverage
+    |> Seq.map (fun (_, _, x) -> x)
+    |> List.ofSeq
+    |> printfn "%A"
+*)
+    [0L..20L] |> Seq.map (
+        fun y -> 
+        let line = (getCoverage coords y |> List.ofSeq)
+        printfn $"%A{line}"
+        gapFinder 0L line
+    ) |> List.ofSeq |> printfn "%A"
+    0L
