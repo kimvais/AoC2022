98c55d5a5e2d7b4bd14dd2dd24c50c91449408d0
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2022-12-16 10:48:06 +0200

    Path generation for day 16

--- Day16.fs
+++ Day16.fs
@@ -3,8 +3,8 @@
 open System.Text.RegularExpressions
 open AoC2022.Utils
 
-type Valve = { Id: string; FlowRate: int64; Neighbours: string list }
 
+type Valve = { Name: string; FlowRate: int64; Neighbours: string list }
 let valveRe =
     Regex @"^Valve ([A-Z]{2}) has flow rate=(\d+); tunnels? leads? to valves? ([A-Z, ]+)"
 
@@ -13,16 +13,34 @@ let reToValve (s) =
     let neighbours = m.Groups.[3].Value |> splitS ", " |> List.ofArray
 
     let valve =
-        { Id = m.Groups.[1].Value
+        { Name = m.Groups.[1].Value
           FlowRate = m.Groups.[2].Value |> int64
           Neighbours = neighbours }
 
     printfn $"%A{valve}"
     valve
+    
 
+let dfs valveMap (startNode: Valve) =
+    let rec dfsHelper (currentNode: Valve) (visited: string list) =
+        let neighbors = currentNode.Neighbours
+        let updatedVisited = currentNode.Name :: visited
+        let unvisitedNeighbors =
+            neighbors |> List.filter (fun neighbor -> not (List.contains (Map.find neighbor valveMap).Name visited))
+        let visitedNeighbors =
+            unvisitedNeighbors |> List.map (fun neighbor -> dfsHelper (Map.find neighbor valveMap) updatedVisited)
+            |> List.concat
+        updatedVisited :: visitedNeighbors
+
+    dfsHelper startNode []
+
+    
 let part1 fn () =
     let input = readInput fn
-    let valves = input |> Seq.map reToValve
-    valves |> Seq.sumBy (fun v -> v.FlowRate)
+    let valves = input |> Seq.map (reToValve >> fun v -> (v.Name, v)) |> Map.ofSeq
+    printfn "%A" valves
+    valves |> Map.values |> Seq.head |> dfs valves |> List.map List.rev |> printfn "%A"
+    // let key = List.head paths
+    0L
 
 let part2 fn () = 0L
