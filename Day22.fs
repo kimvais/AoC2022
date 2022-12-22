100a5b4d9ba105821415dc3fb872f9b06a982880
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2022-12-22 15:11:39 +0200

    Day 22, parsing

--- Day22.fs
+++ Day22.fs
@@ -3,20 +3,33 @@
 open System.Text.RegularExpressions
 open AoC2022.Utils
 
-type Direction =
+type Turn =
     | Left
     | Right
 
-type Instruction =
-    | Left of int
-    | Right of int
+type Instruction = int * Turn
+
+type Direction =
+    | East
+    | South
+    | West
+    | North
+
+type Standing = { Facing: Direction; X: int; Y: int }
+
 
 let makeInstruction (n, dir) =
     match dir with
-    | "R" -> Right n
-    | "L" -> Left n
+    | "R" -> n, Right
+    | "L" -> n, Left
 
 let instructionRe = Regex @"(\d+)([RL])"
+
+let notSpace (_, c) = c <> ' '
+
+let (convert: seq<seq<char>> -> Map<int, char>[]) =
+    Seq.map (Seq.mapi (fun i c -> i, c) >> Seq.filter notSpace >> Map.ofSeq)
+    >> Array.ofSeq
 
 let part1 fn () =
     let input = readInput fn
@@ -32,24 +45,25 @@ let part1 fn () =
 
     let mapWidth = mapInput |> Seq.map String.length |> Seq.max
 
-    let rows =
+    let rows' =
         mapInput
         |> Seq.map (fun s -> s.PadRight mapWidth |> Seq.map char)
 
-    let notSpace (_, c) = c <> ' '
 
-    let cols =
-        rows
-        |> Seq.transpose
-        |> Seq.map (Seq.mapi (fun i c -> i, c) >> Seq.filter notSpace)
+    let cols = rows' |> Seq.transpose |> convert
 
-    let rows' =
-        rows
-        |> Seq.map (Seq.mapi (fun i c -> i, c) >> Seq.filter notSpace)
+    let rows = rows' |> convert
 
-    printfn "%A" rows'
+    printfn "%A" rows
     printfn "%A" cols
     printfn "%A" instructions
+
+    let start =
+        { Facing = East
+          X = (rows |> Seq.head |> Map.keys |> Seq.min)
+          Y = 0 }
+
+    printfn $"%A{start}"
     0L
 
 let part2 fn () = 0L
